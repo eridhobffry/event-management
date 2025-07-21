1bca9a1d929a63eecf65e4b5d39f243c01977868
Eridho Buffery Rollian <eridhobffry@gmail.com>
2025-07-21 17:26:05 +0200

    Add configuration for Tailwind CSS, update Next.js settings for remote images, and introduce new dependencies for form handling and animations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_STACK_PROJECT_ID: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
+    NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY:
+      process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
+    STACK_SECRET_SERVER_KEY: process.env.STACK_SECRET_SERVER_KEY,
+  },
 };
 
 export default nextConfig;
