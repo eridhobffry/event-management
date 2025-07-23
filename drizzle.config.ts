0f68c1fa266a43a35d723ce838651e3b7c20cd95
Eridho Buffery Rollian <eridhobffry@gmail.com>
2025-07-23 10:02:19 +0200

    Refactor database configuration in drizzle.config.ts to use defineConfig and update connection settings; add new database commands to package.json for better management

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema/*",
   out: "./src/db/migrations",
-  driver: "pg", // PostgreSQL driver
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgresql://neondb_owner:npg_SZy1ukJqb3Pm@ep-jolly-surf-a9fejabp-pooler.gwc.azure.neon.tech/neondb?sslmode=require&channel_binding=require",
+  dialect: "postgresql",
+  dbCredentials: {
+    url:
+      process.env.NEON_DATABASE_URL ||
+      "postgresql://neondb_owner:npg_SZy1ukJqb3Pm@ep-jolly-surf-a9fejabp-pooler.gwc.azure.neon.tech/neondb?sslmode=require&channel_binding=require",
+  },
   verbose: true,
   strict: true,
-} satisfies Config;
+});
